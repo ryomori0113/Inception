29405fa6ecb2a7190c27e9c0db56842dc9b180b6
ryomori0113 <rymori0113@gmail.com>
2026-05-06 13:49:04 +0900

    add: mariadb

---
+++ requirements/mariadb/tools/script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MariaDB..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+    /usr/bin/mysqld --user=mysql --bootstrap << EOF
+USE mysql;
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+fi
+
+echo "Starting MariaDB..."
+exec /usr/bin/mysqld --user=mysql --console
