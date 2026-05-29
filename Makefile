c9067ad6f6c06d7e91882d5725935c0659741b62
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2026-05-29 20:58:41 +0900

    add : src

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
-DATA_PATH = ./data
+#DATA_PATH = ./data 修正前
+DATA_PATH = /home/ryomori/data
 
 up:
 	@mkdir -p ${DATA_PATH}/wordpress
 	@mkdir -p ${DATA_PATH}/mariadb
-	docker compose up --build
+	docker compose -f ./srcs/docker-compose.yml up --build
 
 .PHONY: up
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,9 +2,13 @@
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
     wp core download --allow-root
+    # MariaDBが起動して接続できるようになるまで待機
+    sleep 10
     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
 
     wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root #wardpressの初期設定
-    wp user create $MYSQL_USER $MYSQL_EMAIL --role=author --user_pass=$MYSQL_PASSWORD --allow-root #userの追加設定(adomin以外)
+    wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+
+    #wp user create $MYSQL_USER $MYSQL_EMAIL --role=author --user_pass=$MYSQL_PASSWORD --allow-root #userの追加設定(adomin以外)
 fi
 exec php-fpm83 -F
